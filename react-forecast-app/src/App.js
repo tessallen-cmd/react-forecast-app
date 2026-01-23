caabdb647c16a22e2391231867172166b5720c12
tessallen-cmd <tessstablet@gmail.com>
2026-01-23 17:56:32 +0000

    Initial commit

---
+++ react-forecast-app/src/App.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import './App.css';
+import Weather from "./Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from React  </h1>
+        <Weather city="Paris"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-forecast-app/src/Weather.js
@@ -0,0 +1,32 @@
+import React from"react";
+import axios from "axios";
+
+import { Circles } from "react-loader-spinner";
+
+export default function Weather(props){
+    function handleResponse(response){
+   alert(`The weather in ${response.data.name} is ${response.data.main.temp}℃ `) 
+   
+    }
+
+   
+    let apiKey = "f11f933c20efd3709889a2b1a4018580";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    
+      return (
+        <Circles
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      
+      )
+  
+    }
