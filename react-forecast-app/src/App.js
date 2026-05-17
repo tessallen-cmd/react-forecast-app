c046fa5e16f551e5a61378fa95fb52a11a07bfa1
tessallen-cmd <tessstablet@gmail.com>
2026-05-17 11:32:45 +0100

    footer  link to email
    f

--- react-forecast-app/src/App.js
+++ react-forecast-app/src/App.js
@@ -5,12 +5,20 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    
     <div className="App">
       <header className="App-header">
-        <h1>Hello from React  </h1>
-        <Weather city="Paris"/>
+        <h1>Hello from React </h1>
+        <Weather city="Paris" />
       </header>
+
+      <footer>
+        This project was coded by Tess Allen
+        <a
+          href="https://mail.google.com/mail/u/0/#inbox"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      </footer>
     </div>
   );
 }
